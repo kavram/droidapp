a3f7351d8d2c62caefc23f9cfe22df8949d0e096
kavram <kir@test.com>
2014-06-04 23:59:22 -0700

    update

--- UpmileAndroidApp/src/com/upmile/android/MainActivity.java
+++ UpmileAndroidApp/src/com/upmile/android/MainActivity.java
@@ -4,28 +4,94 @@ import java.util.List;
 
 import org.json.JSONObject;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.location.LocationClient;
 import com.upmile.util.FileHelper;
+
+import android.app.Dialog;
+import android.app.DialogFragment;
 import android.app.LoaderManager;
 import android.os.Bundle;
 import android.content.Intent;
+import android.content.IntentSender;
 import android.content.Loader;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ListView;
+import android.widget.Toast;
 import android.support.v4.app.FragmentActivity;
 //import com.google.android.gms.maps.SupportMapFragment;
 
 
-public class MainActivity extends FragmentActivity implements LoaderManager.LoaderCallbacks<List<DealBean>> {
+public class MainActivity extends FragmentActivity implements LoaderManager.LoaderCallbacks<List<DealBean>>, GooglePlayServicesClient.ConnectionCallbacks,
+GooglePlayServicesClient.OnConnectionFailedListener {
 
+	private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
 	
 	private ListView dealsListView = null;
 	private DealsAdapter adapter = null;
+	private LocationClient mLocationClient = null;
 	
+	  public static class ErrorDialogFragment extends DialogFragment {
+	        // Global field to contain the error dialog
+		  private Dialog mDialog;
+	        // Default constructor. Sets the dialog field to null
+	      public ErrorDialogFragment() {
+	    	  super();
+	          mDialog = null;
+	      }
+	      // Set the dialog to display
+	      public void setDialog(Dialog dialog) {
+	    	  mDialog = dialog;
+	      }
+	      // Return a Dialog to the DialogFragment.
+	      @Override
+	      public Dialog onCreateDialog(Bundle savedInstanceState) {
+	    	  return mDialog;
+	      }
+	}
+	
+	  private boolean servicesConnected() {
+	        // Check that Google Play services is available
+	        int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+	        // If Google Play services is available
+	        if (ConnectionResult.SUCCESS == resultCode) {
+	            // In debug mode, log the status
+	            Log.d("Location Updates", "Google Play services is available.");
+	            // Continue
+	            return true;
+	        // Google Play services was not available for some reason
+	        } else {
+	            // Get the error code
+	            //int errorCode = connectionResult.getErrorCode();
+	            // Get the error dialog from Google Play services
+	            Dialog errorDialog = GooglePlayServicesUtil.getErrorDialog(
+	                    resultCode,
+	                    this,
+	                    CONNECTION_FAILURE_RESOLUTION_REQUEST);
+
+	            // If Google Play services can provide an error dialog
+	            if (errorDialog != null) {
+	                // Create a new DialogFragment for the error dialog
+	                ErrorDialogFragment errorFragment = new ErrorDialogFragment();
+	                // Set the dialog in the DialogFragment
+	                errorFragment.setDialog(errorDialog);
+	                // Show the error dialog in the DialogFragment
+	                //errorFragment.show(getSupportFragmentManager(), "Location Updates");
+	            }
+	            return false;
+	        }
+	    }  
+	  
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		mLocationClient = new LocationClient(this, this, this);
+		
 		if(dealsListView == null)
 			dealsListView = (ListView) findViewById(R.id.deals_list);
 		if(adapter == null){
@@ -38,7 +104,7 @@ public class MainActivity extends FragmentActivity implements LoaderManager.Load
 	@Override
 	protected void onStart(){
 		super.onStart();
-	
+		mLocationClient.connect();
 	}
 	
 	@Override
@@ -49,6 +115,7 @@ public class MainActivity extends FragmentActivity implements LoaderManager.Load
 	
 	@Override
 	protected void onStop(){
+		mLocationClient.disconnect();
 		super.onStop();
 	}
 
@@ -137,6 +204,43 @@ public class MainActivity extends FragmentActivity implements LoaderManager.Load
 	@Override
 	public void onLoaderReset(Loader<List<DealBean>> arg0) {
 		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onConnectionFailed(ConnectionResult connectionResult) {
+		if (connectionResult.hasResolution()) {
+            try {
+                // Start an Activity that tries to resolve the error
+                connectionResult.startResolutionForResult(this, CONNECTION_FAILURE_RESOLUTION_REQUEST);
+                /*
+                 * Thrown if Google Play services canceled the original
+                 * PendingIntent
+                 */
+            } catch (IntentSender.SendIntentException e) {
+                // Log the error
+                e.printStackTrace();
+            }
+        } else {
+            /*
+             * If no resolution is available, display a dialog to the
+             * user with the error.
+             */
+           // showErrorDialog(connectionResult.getErrorCode());
+        }
+    }
+		
+	
+
+	@Override
+	public void onConnected(Bundle arg0) {
+		Toast.makeText(this, "Connected", Toast.LENGTH_SHORT).show();
+		
+	}
+
+	@Override
+	public void onDisconnected() {
+		Toast.makeText(this, "Disconnected. Please re-connect.", Toast.LENGTH_SHORT).show();
 		
 	}	
 }
